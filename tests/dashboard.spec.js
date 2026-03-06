c9aa282a512a87389f99df9edf738ac7af1dda14
anbu <anbuv2000@gmail.com>
2026-03-06 14:41:51 +0530

    Re run again

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -1,17 +1,21 @@
 
 import { test, expect } from '@playwright/test';
 import { login } from '../Pages/login';
-import {yosiChat} from '../Pages/yosichat';
+import { yosiChat } from '../Pages/yosichat';
 
 test('Dashboard Login', async ({ page }) => {
 
-  
-  
-  
+
+
+
   //1.Dashboard Login
   const loginPage = new login(page);
 
-  await loginPage.navigateToDashboardPage(page,"https://preprodportal.yosicare.com/index");
+  await loginPage.navigateToDashboardPage(page, "https://preprodportal.yosicare.com/index");
+
+  await page.screenshot({ path: 'failure.png' });
+  console.log(await page.content());
+
   await loginPage.loginToApp('500500qa@yopmail.com', 'Yosi2026$');
   await page.waitForLoadState('networkidle');
   await loginPage.locationSelection();
